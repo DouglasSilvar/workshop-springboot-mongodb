fa7a5879639e9e4f123d790cff5bfffe9b6d6ad8
DouglasSilvar <gogaplay@gmail.com>
2021-06-06 09:48:16 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/example/workshop/resources/PostResource.java
@@ -0,0 +1,40 @@
+package com.example.workshop.resources;
+
+import com.example.workshop.domain.Post;
+import com.example.workshop.domain.User;
+import com.example.workshop.dto.UserDTO;
+import com.example.workshop.services.PostService;
+import com.example.workshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    //@RequestMapping(method = RequestMethod.GET )
+
+
+    @GetMapping(value="/{id}" )
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
+
+
+
+
+
+
+}
